cbeee641dc171b33cfb7e32bf3c5b37d27164ee5
Muhammad Waqas Shahid Khan <mwaqask8040@gmail.com>
2024-12-03 20:23:11 +0500

    Added Database Config

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/database');
 
 dotenv.config();
+connectDB();
 const app = express();
 
 app.use(cors());
