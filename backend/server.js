8a182b6cb17ed5fd63c31723db63d2152eb766d0
Muhammad Waqas Shahid Khan <mwaqask8040@gmail.com>
2024-12-09 06:13:22 +0500

    Vecel App.js URL updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack
+    res.status(500).send('Something broke!'); // Send a 500 response
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
