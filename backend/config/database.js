790065341c7d1b44f96fc08bba5b96bd37bad74b
Muhammad Waqas Shahid Khan <mwaqask8040@gmail.com>
2024-12-09 06:25:29 +0500

    Vecel App.js URL updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI); // No deprecated options here
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
 };
 
 module.exports = connectDB;
